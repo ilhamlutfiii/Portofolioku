eb1bd172016b68abfb07c6ac189cee4bc15cfe53
ilhamlutfiii <ilhamlutfiansyahd4@gmail.com>
2026-08-19 13:50:47 +0700

    Fix port binding for Railway deployment

--- backend/main.go
+++ backend/main.go
@@ -87,7 +87,7 @@ func main() {
 		port = "8080"
 	}
 
-	fmt.Printf("Server Golang berjalan di port: %s\n", port)
+	fmt.Printf("Server Golang berjalan di port:", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
